8873a4736d341f76dc9ded61f795e02c8295a1f2
Cagri Gurleyuk <cgurleyuk@gmail.com>
2017-12-04 14:55:09 +0100

    adds kaiser windowed snr due to problems with iq snr extraction

--- dsm.jl
+++ dsm.jl
@@ -1,7 +1,9 @@
 module dsm
+    export snr, snrx
+
     import DSP
 
-    function snrn(y, f_i, f_bw)
+    function snr_iq(y, f_i, f_bw)
         n   = length(y);
         nbw = trunc(Int64, n*f_bw);
 
@@ -13,8 +15,10 @@ module dsm
         i_sig = (a*xs+b*xc);
         i_noi = y - i_sig;
 
-        w     = DSP.Windows.kaiser(n, 20);
+        w     = DSP.Windows.hanning(n);
         wf    = sum(w.^2)/n;
+        wfa   = sum(w)/n;
+
         I_sig = fft(i_sig.*w)/n;
         I_noi = fft(i_noi.*w)/n;
 
@@ -24,7 +28,27 @@ module dsm
         10*log10(P_sig/P_noi)
     end
 
-    function snr(y, f_i, f_bw, f_s)
-        snrn(y, f_i/f_s, f_bw/f_s);
+    function snr_kaiser(y, f_i, f_bw, f_s)
+        n = length(y);
+
+        sig_bin  = f_i/f_s*n;
+        bw_bin = f_bw/f_s*n;
+        sig_bins = (sig_bin-14:1:sig_bin+16);
+        noi_bins = vcat(1:sig_bin-15, sig_bin+17:bw_bin);
+
+        w = DSP.Windows.kaiser(n, 20);
+        wf    = sum(w.^2)/n;
+        Y = fft(y.*w)/n;
+        P_sig = sum(abs.(Y[trunc.(Int64, sig_bins)]).^2);
+        P_noi = sum(abs.(Y[trunc.(Int64, noi_bins)]).^2);
+
+        10*log10(P_sig/P_noi)
     end
+
+    function snr(y, f_i, f_bw, f_s)
+        # snr_iq(y, f_i/f_s, f_bw/f_s);
+        snr_kaiser(y, f_i, f_bw, f_s);
+    end
+
 end
+
